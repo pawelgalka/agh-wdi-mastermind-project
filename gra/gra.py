c3a0caa4d1ea9ba6e2d3543bbd43ce064c8b8153
pawelgalka <33521206+pawelgalka@users.noreply.github.com>
2017-11-30 00:15:30 +0100

    Add files via upload
    29.11.2017

--- gra/gra.py
+++ gra/gra.py
@@ -3,9 +3,11 @@ import pygame,sys
 FPS = 60
 width = 680
 height = 800
-ball_height=60
+ball_height = 60
+m = False
+m2 = False
 pygame.init()
-m=False
+
 display = pygame.display.set_mode((width, height)) #tuple width,height
 
 pygame.display.set_caption("MASTERMIND") #change title of window
@@ -23,8 +25,14 @@ pink = pygame.image.load("pink.png")
 red = pygame.image.load("red.png")
 deck = pygame.image.load("deck.png")
 field = pygame.image.load("field.png")
+mode1 = pygame.image.load("mode1.png")
+mode2 = pygame.image.load("mode2.png")
+level1 = pygame.image.load("level1.png")
+level2 = pygame.image.load("level2.png")
 
-def menu(m):
+def menu():
+    global mode, m
+    m=False
     while not m:
 
         for event in pygame.event.get():
@@ -32,9 +40,37 @@ def menu(m):
                 sys.exit(0)
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 mouse = pygame.mouse.get_pos()
-                if (322 > mouse[0] > 272) and (370 > mouse[1] > 320):
+                if (540 > mouse[0] > 140) and (300 > mouse[1] > 150):
+                    mode=1
                     m = True
-        pygame.draw.rect(display, (255, 0, 0), (width * 0.4, height * 0.4, 50, 50))
+                if (540 > mouse[0] > 140) and (650 > mouse[1] > 500):
+                    mode=2
+                    m = True
+        display.blit(background, (0, 0))
+        display.blit(mode1, (140, 150))
+        display.blit(mode2, (140, 500))
+
+        pygame.display.update()
+
+def level_menu():
+    global level, m2
+    m2 = False
+    while not m2:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mouse = pygame.mouse.get_pos()
+                if (540 > mouse[0] > 140) and (300 > mouse[1] > 150):
+                    level=1
+                    m2 = True
+                if (540 > mouse[0] > 140) and (650 > mouse[1] > 500):
+                    level=2
+                    m2 = True
+        display.blit(background, (0, 0))
+        display.blit(level1, (140, 150))
+        display.blit(level2, (140, 500))
 
         pygame.display.update()
 
@@ -51,13 +87,19 @@ while True: #main loop
                 x_change = 5
 
 
-            print(event) #display event in a console to check what's happening
+            #print(event) #display event in a console to check what's happening
 
+    if m==False:
+        menu()
 
-    display.fill((255, 255, 0))
+    if m2==False:
+        level_menu()
+
+    #display.fill((255, 255, 0))
     display.blit(background, (0, 0))
-    display.blit(yellow, (width * 0.8, height - 100))
-    display.blit(orange, (width * 0.8, height - 100 - ball_height*1))
+    if level==2:
+        display.blit(yellow, (width * 0.8, height - 100))
+        display.blit(orange, (width * 0.8, height - 100 - ball_height*1))
     display.blit(blue, (width * 0.8, height - 100 - ball_height*2))
     display.blit(green, (width * 0.8, height - 100 - ball_height*3))
     display.blit(turkish, (width * 0.8, height - 100 - ball_height*4))
@@ -69,8 +111,7 @@ while True: #main loop
 
     pygame.display.update()
 
-    menu(m)
-    m=True
+
 
     clock.tick(FPS)
 
